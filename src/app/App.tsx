f9955e0fb712769a0fe4ab3f33340bd0fdff7334
LibrePixelSoftware <121248028+LibrePixelSoftware@users.noreply.github.com>
2023-04-25 12:29:35 +0200

    Add files via upload

---
+++ src/app/App.tsx
@@ -0,0 +1,33 @@
+import type { RouterOnChangeArgs } from 'preact-router'
+import { Router } from 'preact-router'
+import '../styles/global.css'
+import '../styles/nodes.css'
+import { Analytics } from './Analytics.js'
+import { Header } from './components/index.js'
+import { Changelog, Generator, Generators, Guide, Guides, Home, Partners, Sounds, Transformation, Versions, Worldgen } from './pages/index.js'
+import { cleanUrl } from './Utils.js'
+
+export function App() {
+	const changeRoute = (e: RouterOnChangeArgs) => {
+		window.dispatchEvent(new CustomEvent('replacestate'))
+		// Needs a timeout to ensure the title is set correctly
+		setTimeout(() => Analytics.pageview(cleanUrl(e.url)))
+	}
+
+	return <>
+		<Header />
+		<Router onChange={changeRoute}>
+			<Home path="/" />
+			<Generators path="/generators" />
+			<Worldgen path="/worldgen" />
+			<Partners path="/partners" />
+			<Sounds path="/sounds" />
+			<Changelog path="/changelog" />
+			<Versions path="/versions" />
+			<Transformation path="/transformation" />
+			<Guides path="/guides" />
+			<Guide path="/guides/:id" />
+			<Generator default />
+		</Router>
+	</>
+}
---
+++ src/app/Main.tsx
@@ -0,0 +1,25 @@
+import { render } from 'preact'
+import '../styles/global.css'
+import '../styles/nodes.css'
+import { App } from './App.js'
+import { LocaleProvider, ProjectProvider, StoreProvider, ThemeProvider, TitleProvider, VersionProvider } from './contexts/index.js'
+
+function Main() {
+	return (
+		<StoreProvider>
+			<LocaleProvider>
+				<ThemeProvider>
+					<VersionProvider>
+						<TitleProvider>
+							<ProjectProvider>
+								<App />
+							</ProjectProvider>
+						</TitleProvider>
+					</VersionProvider>
+				</ThemeProvider>
+			</LocaleProvider>
+		</StoreProvider>
+	)
+}
+
+render(<Main />, document.body)
